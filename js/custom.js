697df753235cadc78c51aa2a531cdaf17f848199
jadebar <github@jadebar.co.uk>
2025-03-23 22:27:50 +0000

    init

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+
+(function ($) {
+
+"use strict";
+
+  // NAVBAR
+  $('.navbar-collapse a').on('click',function(){
+    $(".navbar-collapse").collapse('hide');
+  });
+  
+  // CUSTOM LINK
+  $('.smoothscroll').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 60;
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+    }
+  });
+
+  // CLICK CLASS
+  $('.service-block-info').click(function(){
+    $(this).find(".front, .back").toggleClass("front back");
+  });
+
+})(window.jQuery);
